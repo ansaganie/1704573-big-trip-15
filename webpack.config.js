1538a84cba27056920834dd8a103a7c7e26ca79e
ansagan <islamgaliev@mail.ru>
2021-07-29 14:02:25 +0500

    Dev server installed

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,10 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    watchContentBase: true,
+    port: 3000
+  }
 }
