cae20de24bc4ed8940a4c6ead63365b069f13796
Ansagan Islamgali <islamgaliev@mail.ru>
2021-08-03 18:30:36 +0500

    event item element now uses inputed object for rendering

--- src/view/event-item.js
+++ src/view/event-item.js
@@ -1,29 +1,39 @@
-export const createEventItem = () =>
-  `<li class="trip-events__item">
+import { createOffer } from './event-item-offer';
+import { formatDate } from '../utils/date';
+
+export const createEventItem = ({
+  type,
+  dateFrom,
+  dateTo,
+  basePrice,
+  offers,
+  destination,
+}) => {
+  const offersFragment = [];
+  offers.forEach((offer) => offersFragment.push(createOffer(offer)));
+
+  return `
+  <li class="trip-events__item">
     <div class="event">
-      <time class="event__date" datetime="2019-03-18">MAR 18</time>
+      <time class="event__date" datetime="${formatDate(dateFrom, 'yyyy-mm-dd')}">${formatDate(dateFrom, 'MMM dd')}</time>
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
       </div>
-      <h3 class="event__title">Taxi Amsterdam</h3>
+      <h3 class="event__title">${type.charAt(0).toUpperCase() + type.slice(1)} ${destination.name}</h3>
       <div class="event__schedule">
         <p class="event__time">
-          <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+          <time class="event__start-time" datetime="${dateFrom}">${formatDate(dateFrom, 'hh:mm')}</time>
           —
-          <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+          <time class="event__end-time" datetime="${dateTo}">${formatDate(dateTo, 'hh:mm')}</time>
         </p>
-        <p class="event__duration">30M</p>
+        <p class="event__duration">${(dateTo - dateFrom)/60000}M</p>
       </div>
       <p class="event__price">
-        €&nbsp;<span class="event__price-value">20</span>
+        €&nbsp;<span class="event__price-value">${basePrice}</span>
       </p>
       <h4 class="visually-hidden">Offers:</h4>
       <ul class="event__selected-offers">
-        <li class="event__offer">
-          <span class="event__offer-title">Order Uber</span>
-          +€&nbsp;
-          <span class="event__offer-price">20</span>
-        </li>
+        ${offersFragment.join('')}
       </ul>
       <button class="event__favorite-btn event__favorite-btn--active" type="button">
         <span class="visually-hidden">Add to favorite</span>
@@ -36,3 +46,4 @@ export const createEventItem = () =>
       </button>
     </div>
   </li>`;
+};
