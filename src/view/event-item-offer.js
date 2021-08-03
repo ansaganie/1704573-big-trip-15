be602b60e57f766539b64e0eb68c4bdaff0b7499
Ansagan Islamgali <islamgaliev@mail.ru>
2021-08-03 18:29:57 +0500

    Offer was moved to separate element

---
+++ src/view/event-item-offer.js
@@ -0,0 +1,6 @@
+export const createOffer = ({title, price}) =>
+  `<li class="event__offer">
+    <span class="event__offer-title">${title}</span>
+    +€&nbsp;
+    <span class="event__offer-price">${price}</span>
+  </li>`;
