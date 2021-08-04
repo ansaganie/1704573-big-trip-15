62416b37f37f051e12683aff73984cf94183fe5a
Ansagan Islamgali <islamgaliev@mail.ru>
2021-08-04 19:12:25 +0500

    Edit form's destination element was moved to separate module;

---
+++ src/view/edit-form-destination.js
@@ -0,0 +1,24 @@
+import { showOrHide } from '../utils/show-hide';
+
+export const createEditFromDestination = ({ description, pictures }) => {
+  let picturesOfDestination = '';
+
+  if (pictures) {
+    pictures.forEach((picture) => {
+      picturesOfDestination = `${picturesOfDestination}
+      <img class="event__photo" src="${picture.src}" alt="${picture.description}">`;
+    });
+  }
+
+  return `
+    <section class="event__section  event__section--destination">
+          <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+          <p class="event__destination-description" ${showOrHide(description)}>${description}</p>
+          <div class="event__photos-container ${showOrHide(description)}">
+            <div class="event__photos-tape">
+              ${picturesOfDestination}
+            </div>
+          </div>
+    </section>
+    `;
+};
