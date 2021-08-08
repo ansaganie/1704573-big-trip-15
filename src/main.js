1c70ee23be693f5485ddc54d6397f0b1b3f447f1
ansagan <islamgaliev@mail.ru>
2021-08-08 20:09:18 +0500

    Event item toggle into edit form and back

--- src/main.js
+++ src/main.js
@@ -6,13 +6,9 @@ import TripSort from './view/trip-sort.js';
 import EventItem from './view/EventList/event-item.js';
 import EditForm from './view/EditForm/edit-form.js';
 import EventList from './view/EventList/event-list.js';
-import Destination from './view/EditForm/destination.js';
 import { createTripInfoDate } from './date.js';
-import { render, RenderPosition, capitalize } from './utils.js';
-import { CITY_NAMES, generatePoints } from './mock/event.js';
-import { offers } from './mock/offer.js';
-import { getRandomDestination } from './mock/destination.js';
-import Offers from './view/EditForm/offer.js';
+import { render, RenderPosition } from './utils.js';
+import { generatePoints } from './mock/event.js';
 
 const EVENT_ITEMS_COUNT = 15;
 
@@ -45,6 +41,7 @@ const tripEventsElement = document.querySelector('.trip-events');
 const tripInfo = new TripInfo(tripInfoTitle, tripInfoDate);
 
 const eventList = new EventList();
+const eventListElement = eventList.getElement();
 
 render(tripInfo.getElement(), new TripCost(tripInfoCost).getElement());
 render(tripMainElement, tripInfo.getElement(), RenderPosition.AFTERBEGIN);
@@ -53,65 +50,25 @@ render(filterElement, new TripFilter().getElement());
 render(tripEventsElement, new TripSort().getElement());
 
 for (let i = 0; i < EVENT_ITEMS_COUNT; i++) {
-  if (i !== 0) {
-    render(eventList.getElement(), new EventItem(randomEvents[i]).getElement());
-    continue;
-  }
-  render(eventList.getElement(), new EditForm(randomEvents[i]).getElement());
+  const eventItem = new EventItem(randomEvents[i]);
+  const editForm = new EditForm(randomEvents[i]);
+  const formElement = editForm.getElement();
+  const eventItemElement = eventItem.getElement();
+  const rollUpButton = eventItemElement.querySelector('.event__rollup-btn');
+
+  const onEditFromSubmit = () => {
+    eventListElement.replaceChild(eventItemElement, formElement);
+  };
+
+  const onRollUpButtonClick = () => {
+    eventListElement.replaceChild(formElement, eventItemElement);
+
+    formElement.addEventListener('submit', onEditFromSubmit);
+  };
+
+  render(eventListElement, eventItem.getElement());
+
+  rollUpButton.addEventListener('click', onRollUpButtonClick);
 }
 
-render(tripEventsElement, eventList.getElement());
-
-const eventEditElement = document.querySelector('.event--edit');
-const eventTypeIconElement =
-  eventEditElement.querySelector('.event__type-icon');
-
-const eventTypeOutputElement = eventEditElement.querySelector(
-  '.event__type-output',
-);
-
-const eventTypeGroupElement =
-  eventEditElement.querySelector('.event__type-group');
-
-const eventDetailsElement = eventEditElement.querySelector('.event__details');
-const eventDestinationInputElement = eventEditElement.querySelector(
-  '.event__input--destination',
-);
-
-const eventTypeToggleElement = eventEditElement.querySelector(
-  '.event__type-toggle',
-);
-
-const onEventTypeGroupChange = (event) => {
-  const selectedEventType = event.target.value;
-
-  eventTypeIconElement.src = `img/icons/${selectedEventType}.png`;
-  eventTypeOutputElement.textContent = capitalize(selectedEventType);
-
-  const updatedOffers = new Offers(offers[selectedEventType]).getElement();
-
-  eventDetailsElement.querySelector('.event__section--offers').remove();
-
-  render(eventDetailsElement, updatedOffers, 'afterbegin');
-
-  eventTypeToggleElement.checked = false;
-};
-
-const onEventDestinationInputChange = (event) => {
-  const inputCityName = event.target.value;
-  let updatedDestination = new Destination('');
-
-  if (CITY_NAMES.includes(inputCityName)) {
-    updatedDestination = new Destination(getRandomDestination(inputCityName));
-  }
-
-  eventDetailsElement.querySelector('.event__section--destination').remove();
-
-  render(eventDetailsElement, updatedDestination.getElement());
-};
-
-eventTypeGroupElement.addEventListener('change', onEventTypeGroupChange);
-eventDestinationInputElement.addEventListener(
-  'change',
-  onEventDestinationInputChange,
-);
+render(tripEventsElement, eventListElement);
