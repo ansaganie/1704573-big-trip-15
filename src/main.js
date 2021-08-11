0ae7dfc75c1a59cf0faf6942e73fef5cfcbbef8f
ansagan <islamgaliev@mail.ru>
2021-08-11 22:40:01 +0500

    Code refactoring and bugfixing

--- src/main.js
+++ src/main.js
@@ -80,6 +80,7 @@ render(filterElement, new TripFilterView());
 render(tripEventsElement, new TripSortView());
 
 for (let i = 0; i < EVENT_ITEMS_COUNT; i++) {
+  console.log(randomEvents[i]);
   renderEventItem(eventList, randomEvents[i]);
 }
 
