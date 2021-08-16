93a75ca4bbb58acb67c056d4354101924c495899
ansagan <islamgaliev@mail.ru>
2021-08-16 22:26:35 +0500

    TripEvent skeleton was created

---
+++ src/presenter/trip-event.js
@@ -0,0 +1,37 @@
+
+class TripEvent {
+  constructor(tripEventConatiner) {
+    this._tripEventConatiner = tripEventConatiner;
+  }
+
+  _renderEventItem() {
+
+  }
+
+  _renderTabs() {
+
+  }
+
+  _renderFilters() {
+
+  }
+
+  _renderNoEventMessage() {
+
+  }
+
+  _renderSort() {
+
+  }
+
+  _renderInfo() {
+
+  }
+
+  _renderEventList() {
+
+  }
+}
+
+export default TripEvent;
+
