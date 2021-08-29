934a6cd6f3a372f32a164167fff9c04933116440
ansagan <islamgaliev@mail.ru>
2021-08-30 01:12:12 +0500

    Adds statistics view

---
+++ src/view/stats.js
@@ -0,0 +1,30 @@
+import AbstractView from './abstract';
+
+const createStatsTemplate = () =>
+  `<section class="statistics">
+    <h2 class="visually-hidden">Trip statistics</h2>
+
+    <div class="statistics__item">
+      <canvas class="statistics__chart" id="money" width="900"></canvas>
+    </div>
+
+    <div class="statistics__item">
+      <canvas class="statistics__chart" id="type" width="900"></canvas>
+    </div>
+
+    <div class="statistics__item">
+      <canvas class="statistics__chart" id="time-spend" width="900"></canvas>
+    </div>
+  </section>`;
+
+class Statistics extends AbstractView {
+  constructor() {
+    super();
+  }
+
+  getTemplate() {
+    return createStatsTemplate();
+  }
+}
+
+export default Statistics;
